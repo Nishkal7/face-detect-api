af38235d2a3c06b5e3b23d4364c9ce5e744b1084
Nishkal <nishkal8463@gmail.com>
2019-10-15 21:56:11 +0530

    Adding API

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai');
+
+
+const app = new Clarifai.App({
+ apiKey: '7650d4e8e3d84817903acecc937eb915'
+})
+
+const handleApiCall = (req,res) => {
+	app.models
+.predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
+.then(data => {
+	res.json(data);
+})
+.catch(err => res.status(400).json('Unable to work with API'))
+}
+
+
+const handleImage = (req,res,db) => {
+	const { id } = req.body;
+	db('users').where('id','=', id)
+	.increment('entries',1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage,
+	handleApiCall
+}
