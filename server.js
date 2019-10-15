4b529e4d1075bb0ff461df5989f0254e4372e3db
Nishkal <36766162+Nishkal7@users.noreply.github.com>
2019-10-16 00:07:42 +0530

    Database code removed

--- server.js
+++ server.js
@@ -9,16 +9,6 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'nishkal',
-    database : 'imagedb'
-  }
-});
-
 
 const app = express();
 
